b1a667c86f8a480bc930c3c3d846bcf58189536b
Ingrid <guid_95@hotmail.com>
2020-05-25 15:54:16 +0200

    eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "linebreak-style": 'off',
+    "react/jsx-props-no-spreading": 'off',
+    "react/sort-comp": 'off',
+    "import/prefer-default-export": 'off'
+  },
+};
