6e34b889d73c2c9a5bc18315b2916226163325eb
Ingrid <guid_95@hotmail.com>
2020-06-04 14:13:00 +0200

    add tests to tour card details component

---
+++ src/components/tour-card/TourCardDetails.test.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TourCardDetails from './TourCardDetails';
+
+const defaultProps = {
+  activity: {
+    totalRating: 4.7,
+    title: 'Tour to some place',
+    totalRatingTitle: '900 Reviews',
+    duration: '1 hour',
+    isGygOriginal: true,
+    price: {
+      min: '65',
+      original: '65',
+      type: 'euro',
+    },
+    cardBannerMessage: 'message',
+    smallDescription: 'This tour is awesome, go for it',
+    freeCancellationFlag: true,
+  },
+  isResult: false,
+};
+
+const { activity } = defaultProps;
+
+const {
+  totalRating,
+  title,
+  totalRatingTitle,
+  price,
+  cardBannerMessage,
+  smallDescription,
+} = activity;
+
+describe('<TourCardDetails />', () => {
+  it('renders component', () => {
+    shallow(<TourCardDetails {...defaultProps} />);
+  });
+
+  describe('when used in result page', () => {
+    const resultProps = { ...defaultProps, isResult: true };
+
+    it('can render free cancellation tag', () => {
+      const wrapper = shallow(<TourCardDetails {...resultProps} freeCancellationFlag />);
+      expect(wrapper.find('.free-cancellation-flag').exists()).toBe(true);
+    });
+
+    it('toggles class', () => {
+      const wrapper = shallow(<TourCardDetails {...resultProps} />);
+      expect(wrapper.find('.show-stars').exists()).toBe(true);
+    });
+
+    it('displays correct text', () => {
+      const wrapper = shallow(<TourCardDetails {...resultProps} />);
+      expect(wrapper.find('.result-description').text()).toBe(smallDescription);
+    });
+  });
+
+  describe('when is home page', () => {
+    it('does not render the description', () => {
+      const wrapper = shallow(<TourCardDetails {...defaultProps} />);
+      expect(wrapper.containsMatchingElement('.result-description')).toEqual(false);
+    });
+
+    it('renders card details message', () => {
+      const wrapper = shallow(<TourCardDetails {...defaultProps} />);
+      expect(wrapper.find('.card-details-message').text()).toBe(cardBannerMessage);
+    });
+
+    it('renders rating stars', () => {
+      const wrapper = shallow(<TourCardDetails {...defaultProps} />);
+      expect(wrapper.find('.title-rating-stars').text()).toBe(totalRatingTitle);
+    });
+
+    it('toggle class', () => {
+      const wrapperHide = shallow(<TourCardDetails {...defaultProps} isResult={false} />);
+      expect(wrapperHide.find('.rating-stars').exists()).toEqual(true);
+    });
+  });
+
+  describe('texts', () => {
+    it('has the correct title displayed', () => {
+      const wrapper = shallow(<TourCardDetails {...defaultProps} />);
+      expect(wrapper.find('.tour-details-title').text()).toBe(title);
+    });
+
+    it('has the correct total rating displayed', () => {
+      const wrapper = shallow(<TourCardDetails {...defaultProps} />);
+      const rating = totalRating.toString();
+      expect(wrapper.find('.total-rating').text()).toBe(rating);
+    });
+
+    it('has the correct price displayed', () => {
+      const wrapper = shallow(<TourCardDetails {...defaultProps} />);
+      expect(wrapper.find('.price').text()).toBe(price.original);
+    });
+  });
+});
--- src/propTypes/activityType.js
+++ src/propTypes/activityType.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
 export const activityPropTypes = PropTypes.shape({
-  imageUrl: PropTypes.string.isRequired,
-  imageAlt: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string,
+  imageAlt: PropTypes.string,
   duration: PropTypes.string.isRequired,
-  averageRating: PropTypes.string.isRequired,
+  averageRating: PropTypes.string,
   totalRating: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   totalRatingTitle: PropTypes.string.isRequired,
