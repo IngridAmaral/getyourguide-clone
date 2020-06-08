ccda24f5d7040aace9b2326360ebf7888acac3d8
Ingrid <guid_95@hotmail.com>
2020-06-08 15:44:49 +0200

    refactors rating stars test

--- src/components/tour-card/RatingStars.test.jsx
+++ src/components/tour-card/RatingStars.test.jsx
@@ -2,24 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import RatingStars from './RatingStars';
 import { ReactComponent as Star } from '../../assets/svgs/star.svg';
+import { ReactComponent as StarHalf } from '../../assets/svgs/star-half.svg';
+import { ReactComponent as StarEmpty } from '../../assets/svgs/star-empty.svg';
 
 const defaultProps = {
-  activity: {
-    totalRating: 900,
-    averageRating: '5',
-    title: 'Tour to some place',
-    totalRatingTitle: '900 Reviews',
-    duration: '1 hour',
-    isGygOriginal: true,
-    price: {
-      min: '65',
-      original: '65',
-      type: 'euro',
-    },
-    cardBannerMessage: 'message',
-    smallDescription: 'This tour is awesome, go for it',
-    freeCancellationFlag: true,
-  },
+  averageRating: '4',
 };
 
 describe('<RatingStars />', () => {
@@ -29,11 +16,31 @@ describe('<RatingStars />', () => {
 
   it('renders rating stars', () => {
     const wrapper = shallow(<RatingStars {...defaultProps} />);
+
     expect(wrapper.find('.stars').exists()).toBe(true);
   });
 
   it('renders 5 stars', () => {
-    const wrapper = shallow(<RatingStars {...defaultProps} />);
+    const wrapper = shallow(<RatingStars averageRating="5" />);
+
     expect(wrapper.find(Star)).toHaveLength(5);
+  });
+
+  it.each(['0', '1', '2', '3', '4', '5'])('renders correctly full stars and empty', (stars) => {
+    const starsEmpty = 5 - Number(stars);
+    const wrapper = shallow(<RatingStars averageRating={stars} />);
+
+    expect(wrapper.find(Star)).toHaveLength(Number(stars));
+    expect(wrapper.find(StarEmpty)).toHaveLength(starsEmpty);
+  });
+
+  it.each(['2.5', '2.9', '4.7'])('renders correctly half stars', (stars) => {
+    const starsEmpty = 4 - Math.floor(Number(stars));
+    const starsFull = 4 - starsEmpty;
+    const wrapper = shallow(<RatingStars averageRating={stars} />);
+
+    expect(wrapper.find(StarHalf)).toHaveLength(1);
+    expect(wrapper.find(StarEmpty)).toHaveLength(starsEmpty);
+    expect(wrapper.find(Star)).toHaveLength(starsFull);
   });
 });
