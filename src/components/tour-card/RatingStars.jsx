d7f67ba566c09b682fb811a609c2fb8ed8ef6251
Ingrid <guid_95@hotmail.com>
2020-06-08 15:43:26 +0200

    refactors ratingStars component to receive 1 prop

--- src/components/tour-card/RatingStars.jsx
+++ src/components/tour-card/RatingStars.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { activityPropTypes } from '../../propTypes/activityType';
+import PropTypes from 'prop-types';
 import './RatingStars.scss';
 import { ReactComponent as Star } from '../../assets/svgs/star.svg';
 import { ReactComponent as StarHalf } from '../../assets/svgs/star-half.svg';
@@ -7,32 +7,24 @@ import { ReactComponent as StarEmpty } from '../../assets/svgs/star-empty.svg';
 
 const rating = [1, 2, 3, 4, 5];
 
-export const renderRatingStars = (averageRating, title) => (rating.map((num) => {
+export const renderRatingStars = (averageRating) => (rating.map((num) => {
   if (averageRating >= num) {
-    return <Star key={title + num} />;
+    return <Star key={num} />;
   }
-  if (num - averageRating < 0.5) {
-    return <StarHalf key={title + num} />;
+  if (num - averageRating < 1) {
+    return <StarHalf key={num} />;
   }
-  return <StarEmpty key={title + num} />;
+  return <StarEmpty key={num} />;
 }));
 
-const RatingStars = ({
-  activity,
-}) => {
-  const {
-    averageRating, title,
-  } = activity;
-
-  return (
-    <div className="stars">
-      {renderRatingStars(averageRating, title)}
-    </div>
-  );
-};
+const RatingStars = ({ averageRating }) => (
+  <div className="stars">
+    {renderRatingStars(averageRating)}
+  </div>
+);
 
 export default RatingStars;
 
 RatingStars.propTypes = {
-  activity: activityPropTypes.isRequired,
+  averageRating: PropTypes.string.isRequired,
 };
