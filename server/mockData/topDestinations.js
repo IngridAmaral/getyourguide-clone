bb14dab0dec820edde15c78f6ccc94d489f7d9c3
Ingrid <guid_95@hotmail.com>
2020-06-09 13:57:19 +0200

    fixes typo

--- server/mockData/topDestinations.js
+++ server/mockData/topDestinations.js
@@ -3,9 +3,8 @@ const { toursBarcelona, toursParis, toursNewYork } = require('./tours');
 const topDestinations = [
   {
     destination: 'Paris',
-    img: 'https://cdn.getyourguide.com/tf/assets/static/deals/city-getaway-paris.jpg',
-    descripton: 'Make eyes at the Mona Lisa and write the story of "that time you went to the City of Love."',
-    bgColor: '#FFD938',
+    img: 'https://cdn.getyourguide.com/img/location/5b1a9b213810d.jpeg/92.jpg',
+    description: 'The City of Love, the City of Lights, whatever you call it, falling in love with Paris is easy. Whether you explore the catacombs or lose yourself in the Louvre, make your visit to the French capital unforgettable.',
     btnTxt: 'Explore things to do',
     activities: toursParis.slice(0, 4),
     url: '/dicovery/paris',
@@ -13,8 +12,7 @@ const topDestinations = [
   {
     destination: 'Barcelona',
     img: 'https://cdn.getyourguide.com/tf/assets/static/deals/city-getaway-barcelona.jpg',
-    descripton: 'Admire Gaudi’s quirky architecture and find the paella recipe you’ll serve to dinner party guests back home.',
-    bgColor: '#CCECFF',
+    description: 'Admire Gaudi’s quirky architecture and find the paella recipe you’ll serve to dinner party guests back home.',
     btnTxt: 'Explore things to do',
     activities: toursBarcelona.slice(0, 4),
     url: '/dicovery/barcelona',
@@ -23,8 +21,7 @@ const topDestinations = [
   {
     destination: 'New York City',
     img: 'https://cdn.getyourguide.com/tf/assets/static/deals/city-getaway-newYorkCity.jpg',
-    descripton: 'Take in the iconic skyline and visit the neighborhood hangouts that you’ve only ever seen on TV.',
-    bgColor: '#FFE5EE',
+    description: 'Take in the iconic skyline and visit the neighborhood hangouts that you’ve only ever seen on TV.',
     btnTxt: 'Explore things to do',
     activities: toursNewYork.slice(0, 4),
     url: '/dicovery/new-york-city',
