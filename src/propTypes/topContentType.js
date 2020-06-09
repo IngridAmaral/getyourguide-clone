d56c7d69cd781139c678e8028a7e0bc6834381c2
Ingrid <guid_95@hotmail.com>
2020-06-09 13:56:55 +0200

    refactor top component prop type

--- src/propTypes/topContentType.js
+++ src/propTypes/topContentType.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import { activitiesPropTypes } from './activityType';
 
-export const topContentPropType = PropTypes.arrayOf(PropTypes.shape({
+export const topLocationPropType = PropTypes.shape({
   activities: activitiesPropTypes.isRequired,
-  bgColor: PropTypes.string.isRequired,
-  btnTxt: PropTypes.string.isRequired,
-  descripton: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
   destination: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
-}));
+});
+
+export const topContentPropType = PropTypes.arrayOf(topLocationPropType);
