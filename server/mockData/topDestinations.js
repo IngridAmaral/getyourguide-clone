c395a4d7b0e5f42797a6c4eb069db9dbf792df55
Ingrid <guid_95@hotmail.com>
2020-06-10 08:56:57 +0200

    updates server to work with the new site format

--- server/mockData/topDestinations.js
+++ server/mockData/topDestinations.js
@@ -1,31 +1,35 @@
 const { toursBarcelona, toursParis, toursNewYork } = require('./tours');
 
-const topDestinations = [
-  {
-    destination: 'Paris',
-    img: 'https://cdn.getyourguide.com/img/location/5b1a9b213810d.jpeg/92.jpg',
-    description: 'The City of Love, the City of Lights, whatever you call it, falling in love with Paris is easy. Whether you explore the catacombs or lose yourself in the Louvre, make your visit to the French capital unforgettable.',
-    btnTxt: 'Explore things to do',
-    activities: toursParis.slice(0, 4),
-    url: '/dicovery/paris',
-  },
-  {
-    destination: 'Barcelona',
-    img: 'https://cdn.getyourguide.com/tf/assets/static/deals/city-getaway-barcelona.jpg',
-    description: 'Admire Gaudi’s quirky architecture and find the paella recipe you’ll serve to dinner party guests back home.',
-    btnTxt: 'Explore things to do',
-    activities: toursBarcelona.slice(0, 4),
-    url: '/dicovery/barcelona',
+const topDestinationParis = {
+  destination: 'Paris',
+  img: 'https://cdn.getyourguide.com/img/location/5b1a9b213810d.jpeg/92.jpg',
+  description: 'The City of Love, the City of Lights, whatever you call it, falling in love with Paris is easy. Whether you explore the catacombs or lose yourself in the Louvre, make your visit to the French capital unforgettable.',
+  activities: toursParis.slice(0, 4),
+  todo: toursParis.slice(4, 8),
+  url: '/dicovery/paris',
+};
 
-  },
-  {
-    destination: 'New York City',
-    img: 'https://cdn.getyourguide.com/tf/assets/static/deals/city-getaway-newYorkCity.jpg',
-    description: 'Take in the iconic skyline and visit the neighborhood hangouts that you’ve only ever seen on TV.',
-    btnTxt: 'Explore things to do',
-    activities: toursNewYork.slice(0, 4),
-    url: '/dicovery/new-york-city',
-  },
-];
+const topDestinationBarcelona = {
+  destination: 'Barcelona',
+  img: 'https://cdn.getyourguide.com/img/location/5b21376f5f336.jpeg/92.jpg',
+  description: "Whether you're staring up in awe at Gaudi's Sagrada Familia or staring down in delight at a plate of tasty tapas, Barcelona will leave you smiling. Discover the best of the Catalan capital on your trip.",
+  activities: toursBarcelona.slice(0, 4),
+  todo: toursBarcelona.slice(4, 8),
+  url: '/dicovery/barcelona',
+};
 
-module.exports = { topDestinations };
+const topDestinationNewYork = {
+  destination: 'New York City',
+  img: 'https://cdn.getyourguide.com/img/location/5c04f6f20650d.jpeg/92.jpg',
+  description: 'Ready to take a bite out of the Big Apple? From American landmarks like the Empire State Building to gospel concerts in Harlem, discover the best New York City has to offer.',
+  activities: toursNewYork.slice(0, 4),
+  todo: toursNewYork.slice(4, 8),
+  url: '/dicovery/new-york-city',
+};
+
+
+module.exports = {
+  topDestinationParis,
+  topDestinationBarcelona,
+  topDestinationNewYork,
+};
