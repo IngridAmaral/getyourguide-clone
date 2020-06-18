bb26b82820f9177c2d53ab55f4a2778feb4c103d
Ingrid <guid_95@hotmail.com>
2020-06-18 16:03:22 +0200

    Updates

---
+++ precache-manifest.f060736664d3204ee280900491dcd1e3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6438681a258a081b6e76b2f27fe83f9",
+    "url": "/getyourguide-clone/index.html"
+  },
+  {
+    "revision": "bd8d0d02a58d894ea39e",
+    "url": "/getyourguide-clone/static/css/main.d4073b12.chunk.css"
+  },
+  {
+    "revision": "8b1914d7d581ac19dcdc",
+    "url": "/getyourguide-clone/static/js/2.2728d02b.chunk.js"
+  },
+  {
+    "revision": "2ef8adaa6cc38af660c5134c9f805ef3",
+    "url": "/getyourguide-clone/static/js/2.2728d02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8d0d02a58d894ea39e",
+    "url": "/getyourguide-clone/static/js/main.10883cf3.chunk.js"
+  },
+  {
+    "revision": "24d60b8886cc617fc4da",
+    "url": "/getyourguide-clone/static/js/runtime-main.c737d019.js"
+  },
+  {
+    "revision": "a8a096a5e04a2f59ef0fede217ca3315",
+    "url": "/getyourguide-clone/static/media/basket.a8a096a5.svg"
+  },
+  {
+    "revision": "d88c71b7b24c308a670ae1f488e5ce75",
+    "url": "/getyourguide-clone/static/media/caret.d88c71b7.svg"
+  },
+  {
+    "revision": "2b1c1468f8a7db5bd48f83d49cc661f8",
+    "url": "/getyourguide-clone/static/media/duration.2b1c1468.svg"
+  },
+  {
+    "revision": "829a43f9f06bf5ddd210efbc40364268",
+    "url": "/getyourguide-clone/static/media/gyg-logo.829a43f9.svg"
+  },
+  {
+    "revision": "41700498b97e9fb0b9250246fd2f6fe9",
+    "url": "/getyourguide-clone/static/media/help.41700498.svg"
+  },
+  {
+    "revision": "dbc613f0f82ac5daa80930907e26c10a",
+    "url": "/getyourguide-clone/static/media/like-home.dbc613f0.svg"
+  },
+  {
+    "revision": "5c74f2943a48fa3284dbd67742192653",
+    "url": "/getyourguide-clone/static/media/like.5c74f294.svg"
+  },
+  {
+    "revision": "460fe36b9d98157bb35e23467b75285c",
+    "url": "/getyourguide-clone/static/media/search.460fe36b.svg"
+  },
+  {
+    "revision": "b6db919ccb598e9e100cefc7eba842e7",
+    "url": "/getyourguide-clone/static/media/star-empty.b6db919c.svg"
+  },
+  {
+    "revision": "c3fd44cc7b65dd8ba4f089893b0d2344",
+    "url": "/getyourguide-clone/static/media/star-half.c3fd44cc.svg"
+  },
+  {
+    "revision": "ee1e796ee27a49a4bce2c318e72c97fd",
+    "url": "/getyourguide-clone/static/media/star.ee1e796e.svg"
+  },
+  {
+    "revision": "446f3ac34ea557d71381a62f080505f8",
+    "url": "/getyourguide-clone/static/media/time.446f3ac3.svg"
+  },
+  {
+    "revision": "2e11e50af36243871f8e431688da91b4",
+    "url": "/getyourguide-clone/static/media/user.2e11e50a.svg"
+  },
+  {
+    "revision": "d01fb1d2d9e61cad74439ec0c479780b",
+    "url": "/getyourguide-clone/static/media/zigzag.d01fb1d2.svg"
+  }
+]);
