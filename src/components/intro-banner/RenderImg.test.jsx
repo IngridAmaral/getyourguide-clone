e9301ff0c8c4952e034c7090bc1bc671c60f4e4d
Ingrid <guid_95@hotmail.com>
2020-06-26 17:32:43 +0200

    creates a component that renders the images

---
+++ src/components/intro-banner/RenderImg.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import RenderImg from './RenderImg';
+
+describe('<RenderImg />', () => {
+  it('renders component', () => {
+    shallow(<RenderImg />);
+  });
+
+  it('renders TopLocationCard component with props', () => {
+    jest.useFakeTimers();
+    const wrapper = shallow(<RenderImg />);
+
+    expect(wrapper.find('img').at(0).hasClass('show')).toBe(true);
+    expect(wrapper.find('img').at(1).hasClass('hide')).toBe(true);
+    expect(wrapper.find('img').at(2).hasClass('hide')).toBe(true);
+
+    jest.runOnlyPendingTimers();
+    wrapper.update();
+
+    expect(wrapper.find('img').at(0).hasClass('hide')).toBe(true);
+    expect(wrapper.find('img').at(1).hasClass('show')).toBe(true);
+    expect(wrapper.find('img').at(2).hasClass('hide')).toBe(true);
+
+    jest.useRealTimers();
+  });
+});
