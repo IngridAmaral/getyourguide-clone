b6edb1756fed13e15bce5a557bf72062c1efc668
Ingrid <guid_95@hotmail.com>
2020-07-01 23:46:59 +0200

    fixes path in get destinations tours

--- src/service/destinationsTours.js
+++ src/service/destinationsTours.js
@@ -1,6 +1,6 @@
 import { api } from '../api/api';
 
 export const destinationsTours = async (path) => {
-  const response = await api.get(`destinations/${path}`);
+  const response = await api.get(`destinations/tours-${path}`);
   return response.data;
 };
