ee306e3da3703d0dc01d04e4e9d95a63b52b56c8
Ingrid <guid_95@hotmail.com>
2020-07-10 12:00:49 +0200

    refactors header component to receive props

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import './Header.scss';
+import PropTypes from 'prop-types';
 import { ReactComponent as Logo } from '../../assets/svgs/gyg-logo.svg';
 import Navigation from './Navigation';
+import Search from '../search/Search';
 
 
-const Header = () => (
-  <div className="header-container">
-    <Logo />
-    <Navigation />
+const Header = ({ showSearchBar, enforceCurrencyAndLang }) => (
+  <div className={`header-container${showSearchBar ? ' show-search-bar-header' : ''}`}>
+    <div className="navigation-bar">
+      <div className="left">
+        <Logo />
+        {showSearchBar && (
+          <div className="top-search-bar">
+            <Search simplified />
+          </div>
+        )}
+      </div>
+      <Navigation enforceCurrencyAndLang={enforceCurrencyAndLang} />
+    </div>
+    {showSearchBar && (
+      <div className="bottom-search-bar">
+        <Search simplified />
+      </div>
+    )}
   </div>
 );
+
+
+Header.propTypes = {
+  showSearchBar: PropTypes.bool,
+  enforceCurrencyAndLang: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  showSearchBar: false,
+  enforceCurrencyAndLang: false,
+};
 
 export default Header;
